9de4cfebd0ddac925a7cc3823a7b262c95c38b3a
mikaela-nilsson <mdjnilsson@gmail.com>
2024-12-19 12:49:58 +0100

    välkommen

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace Grupp23_CV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    }
+}
